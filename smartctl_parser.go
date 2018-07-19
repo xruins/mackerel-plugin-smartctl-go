e03850cf3e236218a8f48e6cfa9bf8b78b36dd54
Hikaru Watanabe <hikaru.watanabe@dena.com>
2018-07-19 19:20:30 +0900

    tired

--- smartctl_parser.go
+++ smartctl_parser.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+
+	_ "github.com/xruins/mackerel-plugin-smart-go"
 )
 
 type SmartctlParser struct {
@@ -13,7 +15,7 @@ type SmartctlParser struct {
 }
 
 const (
-	SMARTCTL_CMD              = "smartctl -a %s"
+	SMARTCTL_CMD              = "smartctl -A %s %s"
 	HDPARM_SPINDOWN_CHECK_CMD = "hdparm -C {%s}"
 )
 
@@ -21,20 +23,53 @@ var regexpSmartctl = regexp.MustCompile(`\s*(\d+)\s+([A-Za-z_]+)\s+([x0-9]+)\s+(
 var regexpHdparm = regexp.MustCompile(`([a-z\/]+):\n\s+drive state is:\s+(.+)`)
 
 func (s *SmartctlParser) Get() {
-	devices := make([]string, Len(s.Devices))
-	for device, _ := range s.Devices {
-		devices := append(devices, device)
+	var devicesStatus map[string]string
+
+	// set the statistics of device sleep state.
+	// if it is configured to check SMART even if device sleeps,
+	// filled standy statuses with false.
+	if s.SkipOnSpindown {
+		// fetch whether devices sleeps or not.
+		devices := make([]string, Len(s.Devices))
+		for device, _ := range s.Devices {
+			devices := append(devices, device)
+		}
+		devicesStatus = GetDevicesStatus(s.Devices)
+	} else {
+
+		for device, standby := range devicesStatus {
+			devicesStatus[device] = false
+		}
 	}
-	devicesStatus := GetDevicesStatus(s.Devices)
 }
 
+func GetSmartMetrics(devicesMap map[string]string) []*SmartctlMetric {
+	var metrics = make(*SmartctlMetric, Len(devicesMap))
+	for device, dmiType := range devicesMap {
+		var dmiTypeOption string
+		if dmiType != nil {
+			dmiTypeOption = fmt.Sprintf("-d %s", dmiType)
+		} else {
+			dmiTypeOption = ""
+		}
+		cmd := fmt.Sprintf(SMARTCTL_CMD, device, dmiTypeOption)
+		result, err := os.Exec(cmd)
+		metrics = append(metrics, parseSmartctl)
+	}
+}
+
+func parseSmartctl(s string) (*SmartctlMetric, err) {
+	re := regexpSmartctl.Copy()
+	matches := re.FindAll(s, -1)
+	for matches
+}
 func GetDevicesStatus(devices []string) (bool, err) {
-	cmd := fmt.Sprintf(SMARTCTL_CMD, strings.Join(devices, ","))
+	cmd := fmt.Sprintf(HDPARM_SPINDOWN_CHECK_CMD, strings.Join(devices, ","))
 	result, err := os.Exec(cmd)
 	if err != nil {
 		return nil, errors.New(err)
 	}
-	re := HDPARM_SPINDOWN_CHECK_CMD.Copy()
+	re := regexpHdparm.Copy()
 	// output of hdparm is such as`/dev/sda:
 	// drive state is: standby`.
 	// Then the first subsequence will be path to device,

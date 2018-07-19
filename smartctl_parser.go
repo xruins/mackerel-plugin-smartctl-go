adeb90fe92f0b1cdae3e2c105c68e65da675f533
Hikaru Watanabe <hikaru.watanabe@dena.com>
2018-07-19 18:38:16 +0900

    WIP

--- smartctl_parser.go
+++ smartctl_parser.go
@@ -21,25 +21,38 @@ var regexpSmartctl = regexp.MustCompile(`\s*(\d+)\s+([A-Za-z_]+)\s+([x0-9]+)\s+(
 var regexpHdparm = regexp.MustCompile(`([a-z\/]+):\n\s+drive state is:\s+(.+)`)
 
 func (s *SmartctlParser) Get() {
-	for device, device_type := range s {
-		if isSpindown(device) {
-			continue
-		}
-		parseSmartctl(result)
+	devices := make([]string, Len(s.Devices))
+	for device, _ := range s.Devices {
+		devices := append(devices, device)
 	}
+	devicesStatus := GetDevicesStatus(s.Devices)
 }
 
-func isSpindown(devices []string) (bool, err) {
+func GetDevicesStatus(devices []string) (bool, err) {
 	cmd := fmt.Sprintf(SMARTCTL_CMD, strings.Join(devices, ","))
 	result, err := os.Exec(cmd)
 	if err != nil {
 		return nil, errors.New(err)
 	}
 	re := HDPARM_SPINDOWN_CHECK_CMD.Copy()
-	isSleep := re.Find(result)
-	if isSleep != "standby" {
-		return true
-	} else {
-		return false
+	// output of hdparm is such as`/dev/sda:
+	// drive state is: standby`.
+	// Then the first subsequence will be path to device,
+	// the second one will be status of disk.
+	matches := re.FindAll(result, -1)
+	if matches == nil {
+		return nil, fmt.Errorf("The output of `%s` does not match regexp.", cmd)
 	}
+	var res map[string]bool
+	for _, submatches := range matches {
+		device := submatches[1]
+		if matches[2] == "standby" {
+			standby := true
+		} else {
+			standby := false
+		}
+		res[device] = standby
+	}
+
+	return res, nil
 }
